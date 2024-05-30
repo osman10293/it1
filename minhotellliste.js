3a660b332f0d306a489bcaa04f95769fb212a335
osman10293 <145428102+osman10293@users.noreply.github.com>
2024-05-30 13:00:49 +0200

    Add files via upload

--- minhotellliste.js
+++ minhotellliste.js
@@ -1,9 +1,9 @@
 let Hoteller = {
     "HotellArray": [
-        { "navn": "Heinster", "tlf": "61284000", "epost": "post@sgh.no", "temperatur": [1,3,3,4,5,5,4] },
-        { "navn": "Gaustatoppen", "tlf": "61294940", "epost":"post@rondablikk.no", "temperatur": [1,2,3,4,5,5,4]},
-        { "navn": "Hedlo", "tlf": "61284000", "epost": "post@rondane.no","temperatur": [1,2,3,4,5,5,4]  },
-        { "navn": "Leirvassbu", "tlf": "61284000", "epost": "post@brekkeseter.no","temperatur": [1,2,3,4,5,5,4]  },
-        { "navn": "Memurubu", "tlf": "61284000", "epost": "fjellstua@hjerkinn.no" ,"temperatur": [1,2,3,4,5,5,4]  },
+        { "navn": "Heinster", "tlf": "61284000", "epost": "post@sgh.no", "temperatur": [1,9,9,4,10,8,12] },
+        { "navn": "Gaustatoppen", "tlf": "61294940", "epost":"post@rondablikk.no", "temperatur": [1,5,7,9,10,12,13]},
+        { "navn": "Hedlo", "tlf": "61284000", "epost": "post@rondane.no","temperatur": [10,16,19,19,14,7,16]  },
+        { "navn": "Leirvassbu", "tlf": "61284000", "epost": "post@brekkeseter.no","temperatur": [4,5,6,7,8,9,10]  },
+        { "navn": "Memurubu", "tlf": "61284000", "epost": "fjellstua@hjerkinn.no" ,"temperatur": [7,7,15,14,25,15,14]  },
     ]
 }
