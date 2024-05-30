02691f735145d7a9e29606b52ea7c6ce54a88861
osman10293 <145428102+osman10293@users.noreply.github.com>
2024-05-30 12:44:28 +0200

    Add files via upload

---
+++ minlistekort.js
@@ -0,0 +1,9 @@
+let visittKort = {
+    "kortArray": [
+        { "navn": "osman", "gender": "gutt", "career": "football", "date": "12-1-1111", "nummer": "825515452" },
+        { "navn": "ali", "gender": "gutt", "career": "worker", "date": "12-1-2009", "nummer": "8255" },
+        { "navn": "jamac", "gender": "gutt", "career": "fixer", "date": "1-5-1528", "nummer": "8215452" },
+        { "navn": "dahir", "gender": "gutt", "career": "gamer", "date": "1-0-1562", "nummer": "825661452" },
+        { "navn": "xakim", "gender": "gutt", "career": "filosofi", "date":"2-1-8875", "nummer": "25515452" },
+        { "navn": "ole ", "gender": "gutt", "career": "kriger", "date": "2-4-1258", "nummer": "1545223" },]
+}
