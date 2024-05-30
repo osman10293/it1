80d2f65b38fee9cc91471cb0211a5db12eb2b076
osman10293 <145428102+osman10293@users.noreply.github.com>
2024-05-30 12:52:24 +0200

    Add files via upload

---
+++ minhotellliste.js
@@ -0,0 +1,9 @@
+let Hoteller = {
+    "HotellArray": [
+        { "navn": "Heinster", "tlf": "61284000", "epost": "post@sgh.no", "temperatur": [1,3,3,4,5,5,4] },
+        { "navn": "Gaustatoppen", "tlf": "61294940", "epost":"post@rondablikk.no", "temperatur": [1,2,3,4,5,5,4]},
+        { "navn": "Hedlo", "tlf": "61284000", "epost": "post@rondane.no","temperatur": [1,2,3,4,5,5,4]  },
+        { "navn": "Leirvassbu", "tlf": "61284000", "epost": "post@brekkeseter.no","temperatur": [1,2,3,4,5,5,4]  },
+        { "navn": "Memurubu", "tlf": "61284000", "epost": "fjellstua@hjerkinn.no" ,"temperatur": [1,2,3,4,5,5,4]  },
+    ]
+}
